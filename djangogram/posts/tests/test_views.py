29d19f522c20b52eb14833073cd7e2fd779a023b
supin2 <supine94@gmail.com>
2021-12-13 00:31:46 +0900

    Creating unit test code for get request of creating posts

---
+++ djangogram/posts/tests/test_views.py
@@ -0,0 +1,10 @@
+from django.urls import reverse
+from django.test import TestCase
+
+class TestPosts(TestCase):
+    def test_get_posts_page(self):
+        url = reverse('posts:post_create')
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'posts/post_create.html')
