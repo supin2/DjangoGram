5d92d5720ac4a41b20603f27cd44db8ddca8aa07
supin2 <supine94@gmail.com>
2021-12-14 22:50:10 +0900

    Create select query for feed page in post views

--- djangogram/posts/models.py
+++ djangogram/posts/models.py
@@ -21,7 +21,13 @@ class Post(TimeStamedModel):
     ) 
     image = models.ImageField(blank=False)
     caption = models.TextField(blank=False)
-    image_likes = models.ManyToManyField(user_model.User, related_name='post_image_likes')
+    image_likes = models.ManyToManyField(
+                    user_model.User,
+                    blank=True,
+                    related_name='post_image_likes'
+                )
+    def __str__(self):
+        return f"{self.author}: {self.caption}"
 
 # 댓글 관리
 class Comment(TimeStamedModel):
@@ -38,3 +44,6 @@ class Comment(TimeStamedModel):
         related_name='comment_post'
     )
     contents = models.TextField(blank=True)
+
+    def __str__(self):
+        return f"{self.author}: {self.contents}"
