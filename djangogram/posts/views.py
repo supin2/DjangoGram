91b1e4530c5aa490759da2592eee0d3237d983da
supin2 <supine94@gmail.com>
2021-12-15 22:23:54 +0900

    Implement create comment function

--- djangogram/posts/views.py
+++ djangogram/posts/views.py
@@ -1,14 +1,17 @@
 from django.db.models import Q
-from django.shortcuts import get_object_or_404, render
 from djangogram.users.models import User as user_model
+from django.shortcuts import get_object_or_404, redirect, render, redirect
+from django.urls import reverse
 
 from . import models, serializers
-from .forms import CreatePostForm
+from .forms import CreatePostForm, CommentForm
 
 # Create your views here.
 def index(request):
     if request.method == 'GET':
         if request.user.is_authenticated:
+            comment_form = CommentForm()
+
             user = get_object_or_404(user_model, pk=request.user.id)
             following = user.following.all()
             posts = models.Post.objects.filter(
@@ -16,9 +19,9 @@ def index(request):
             )
 
             serializer = serializers.PostSerializer(posts, many=True) # 피드에 보여지는 포스트가 여러개일 수도 있으니 many=True 옵션 추가
-            print(serializer.data)
+            # print(serializer.data)
 
-            return render(request, 'posts/main.html', {"posts": serializer.data})
+            return render(request, 'posts/main.html', {"posts": serializer.data, "comment_form": comment_form})
     
     return render(request, 'posts/main.html')
 
@@ -50,6 +53,22 @@ def post_create(request):
                 print(form.errors)
 
             return render(request, 'posts/main.html')
+
+        else:
+            return render(request, 'users/main.html')
+
+def comment_create(request, post_id):
+    if request.user.is_authenticated:
+        post = get_object_or_404(models.Post, pk=post_id)
+
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.author = request.user
+            comment.posts = post
+            comment.save()
+
+            return redirect(reverse('posts:index') + "#comment-" + str(comment.id))
 
         else:
             return render(request, 'users/main.html')
